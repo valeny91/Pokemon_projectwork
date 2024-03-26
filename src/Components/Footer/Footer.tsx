de94a4b8bdbcccb69e364cd4bf6744ff5e0e2250
valeny91 <valeny91@gmail.com>
2024-03-26 15:14:11 +0100

    add footer

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <>
+
+      <div className='footer'>
+      <div className='logo'>
+   <img  className='logofooter' src="https://cdn-icons-png.flaticon.com/128/361/361998.png" alt="" />
+   </div>
+
+        
+        <div className='creditsfooter'>
+<p>Vincenzo, Stefania e Valentina Inc.</p>
+        </div>
+
+
+   
+   <div className='iconsfooter'>
+  <a  className='linkedin' href="#">
+  <i className="ri-linkedin-fill"></i>
+  </a>
+  <a className='facebook' href="#">
+    
+  <i className="ri-facebook-fill"></i>
+  </a>
+   </div>
+   
+      </div>
+    </>
+  )
+}
+
+export default Footer
+
--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
+import Footer from '../../Components/Footer/Footer'
+import './MainPage.css'
 
 const MainPage = () => {
   return (
-    <div>
+    <div className='mainPage'>
       <Navbar/>
+      <div className='main'></div>
+      <Footer/>
     </div>
   )
 }
